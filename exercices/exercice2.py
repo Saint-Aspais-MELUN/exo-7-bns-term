0a4601bf5229192bfdd3a538aca7cc45b28cb6b7
COILHAC <62248137+Mireille77@users.noreply.github.com>
2021-02-18 21:02:04 +0100

    Update exercice2.py

--- exercices/exercice2.py
+++ exercices/exercice2.py
@@ -1,9 +1,18 @@
-def tri_iteratif(tab):
-    for k in range( ... , 0, -1):
-        imax = ...
-        for i in range(0 , ... ):
-            if tab[i] > ... :
-                imax = i
-        if tab[max] > ... :
-            ... , tab[imax] = tab[imax] , ...
-    return tab
+liste_eleves = ['a','b','c','d','e','f','g','h','i','j']
+liste_notes = [1, 40, 80, 60, 58, 80, 75, 80, 60, 24]
+
+def meilleures_notes():
+    note_maxi = 0
+    nb_eleves_note_maxi = ...
+    liste_maxi =  ...
+
+    for compteur in range(...):
+        if liste_notes[compteur] == ...:
+            nb_eleves_note_maxi = nb_eleves_note_maxi + 1
+            liste_maxi.append(liste_eleves[...])
+        if liste_notes[compteur] > note_maxi:
+            note_maxi = liste_notes[compteur]
+            nb_eleves_note_maxi = ...
+            liste_maxi = [...]
+
+    return (note_maxi,nb_eleves_note_maxi,liste_maxi)
